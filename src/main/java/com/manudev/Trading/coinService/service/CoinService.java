4eb13ac1ca64b9dd98cb6b8128429b1b6c06a7fd
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2025-04-01 14:15:02 -0300

    impl

--- src/main/java/com/manudev/Trading/coinService/service/CoinService.java
+++ src/main/java/com/manudev/Trading/coinService/service/CoinService.java
@@ -1,6 +1,8 @@
 package com.manudev.Trading.coinService.service;
 
+import com.manudev.Trading.coinService.coinRepository.CoinRepository;
 import com.manudev.Trading.coinService.model.Coin;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
--- src/main/java/com/manudev/Trading/coinService/service/impl/CoinServiceImpl.java
+++ src/main/java/com/manudev/Trading/coinService/service/impl/CoinServiceImpl.java
@@ -1,7 +1,67 @@
 package com.manudev.Trading.coinService.service.impl;
 
+import com.manudev.Trading.coinService.coinRepository.CoinRepository;
+import com.manudev.Trading.coinService.model.Coin;
+import com.manudev.Trading.coinService.service.CoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
-public class CoinServiceImpl {
+public class CoinServiceImpl implements CoinService {
+
+    @Autowired
+    private CoinRepository coinRepository;
+
+    @Override
+    public List<Coin> getCoinList(int page) {
+        String url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10&page="+page;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        }
+        return List.of();
+    }
+
+    @Override
+    public String getMatketChart(String coinId, int days) {
+        return "";
+    }
+
+    @Override
+    public String getCoindDetails(String coinId) {
+        return "";
+    }
+
+    @Override
+    public Coin findById(String coinId) {
+        return null;
+    }
+
+    @Override
+    public String searchCoin(String keyword) {
+        return "";
+    }
+
+    @Override
+    public String getTop50CoinsByMarketCapRank() {
+        return "";
+    }
+
+    @Override
+    public String GetTreadingCoins() {
+        return "";
+    }
 }
